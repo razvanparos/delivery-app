b877474f62a228d55f74b3d075f3c4d0696edaeb
Razvan Paros <razvanparos21@gmail.com>
2024-04-27 03:16:58 +0300

    add new restaurants option

--- src/pages/dashboardRestaurant/dashboardRestaurant.jsx
+++ src/pages/dashboardRestaurant/dashboardRestaurant.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './dashboardRestaurant.css';
 import { useEffect, useState } from 'react';
-import {getDocs, collection, query, where, doc, setDoc} from 'firebase/firestore';
+import {getDocs, collection, query, where, doc, setDoc, addDoc} from 'firebase/firestore';
 import {db} from '../../firebase-config'
 import Loader from '../../components/Loader/Loader';
 import RestaurantCard from '../../components/RestaurantCard/RestaurantCard';
+import {storage} from '../../firebase-config'
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 
 
@@ -15,6 +17,7 @@ function DashboardRestaurant() {
   const [addRestaurant, setAddRestaurant]=useState(false);
   const [restaurantData, setRestaurantData]=useState([]);
   const [loading, setLoading] = useState(false)
+  const [newRestaurantName, setNewRestaurantName] = useState('')
 
   const restaurantsDb = collection(db,'Restaurants')
 
@@ -54,8 +57,6 @@ function DashboardRestaurant() {
         console.log(restaurantData)
     },[restaurantData])
     
-
-
   const signOut = () =>{
     localStorage.setItem('LoggedIn',false)
     localStorage.setItem('RememberMe',false)
@@ -63,15 +64,46 @@ function DashboardRestaurant() {
     navigate('/');
 }
 
+  const addNewRestaurant = async (e)=>{
+      e.preventDefault()
+      let currentUID = localStorage.getItem('currentUserId')
+      const fileInput = document.getElementById('fileInput');
+      const file = fileInput.files[0]; 
+      console.log(file)
+      const imageRef = ref(storage, newRestaurantName);
+      try {
+        await uploadBytes(imageRef, file);
+        console.log("Image uploaded successfully!");
+
+   
+        const imageUrl = await getDownloadURL(imageRef);
+
+
+        await setDoc(doc(db, "Restaurants", currentUID), {
+            name: newRestaurantName,
+            image: imageUrl, 
+            owner: currentUID
+        });
+
+        console.log("Restaurant added successfully!");
+        setAddRestaurant(false); 
+    } catch (error) {
+        console.error("Error uploading image:", error);
+    }
+      
+  }
+
   return (
     <div className='dashboard-restaurant-div dashboard'>
         {addRestaurant ? 
           <form className='add-restaurant-form'>
-            <label htmlFor="">Name</label>
-            <input type="text" />
-            <label htmlFor="">Image</label>
-            <input type="file" />
+            <label htmlFor="">Name </label>
+            <input type="text" value={newRestaurantName}  onChange={(e)=>{setNewRestaurantName(e.target.value)}}/>
 
+            <label htmlFor="">Image </label>
+            <input type="file" id='fileInput' onChange={(e)=>{console.log(e.target.elements)}}/>
+
+            <button onClick={addNewRestaurant}>Submit</button>
             <button onClick={()=>{setAddRestaurant(false)}}>Cancel</button>
           </form> :
             <div className='restaurants-list'>
