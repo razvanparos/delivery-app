58804aba375bb835ae8bd2abd6f7bd1c410d1ccf
Razvan Paros <razvanparos21@gmail.com>
2024-04-29 16:49:38 +0300

    ui changes

--- src/pages/restaurantIndividual/restaurantIndividual.jsx
+++ src/pages/restaurantIndividual/restaurantIndividual.jsx
@@ -5,7 +5,9 @@ import {db} from '../../firebase-config'
 import { useEffect, useState } from 'react';
 import { FaSquarePlus } from "react-icons/fa6";
 import ProductCard from '../../components/ProductCard/ProductCard';
-
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { useNavigate } from 'react-router-dom';
+import Loader from '../../components/Loader/Loader';
 
 
 
@@ -14,43 +16,60 @@ function RestaurantIndividual(){
     const [individualData, setIndividualData]=useState();
     const [userType, setUserType]=useState(localStorage.getItem('userType'));
     const restaurantsDb = collection(db,'Restaurants')
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate();
 
     useEffect(()=>{
         const getIndividualData = async () =>{
             try{
+                setLoading(true)
                 const docRef = doc(db, 'Restaurants',restaurantId)
                 const querySnapshot = await getDoc(docRef);
                 setIndividualData(querySnapshot.data())
+                setLoading(false)
                 } catch(err){
+                    setLoading(false)
                     console.log(err)
                 }
         }
         getIndividualData();
     },[])
+
+    const back = () =>{
+       navigate('/dashboard-restaurant')
+    }
     // useEffect(()=>{
     //     console.log(individualData)
     // },[individualData])
     
     return(
         <div className='restaurant-individual-div'>
-            <img src={individualData?.image} alt="" className='individual-image'/>
-            <h2 className='restaurant-name'>{individualData?.name}</h2>
-            {userType==='restaurant' ?
-                <button className='add-product-btn'>
-                    Add product<FaSquarePlus className='add-product-icon'/>
-                </button>: ''}
+            {loading ? <Loader/> : 
+            <div className='restaurant-individual-div'>
+                <div className='individual-back' onClick={back}>
+                    <FaArrowLeftLong />
+                </div>
+                <img src={individualData?.image} alt="" className='individual-image'/>
+                <h2 className='restaurant-name'>{individualData?.name}</h2>
+                {userType==='restaurant' ?
+                    <button className='add-product-btn'>
+                        Add product<FaSquarePlus className='add-product-icon'/>
+                    </button>: ''}
 
-            <div className='products-list'>
-                {individualData?.products?.map((product)=>{
-                    return <ProductCard
-                                key={product.id}
-                                name={product.name}
-                                price={product.price}
-                                image={product.image}
-                            />
-                })}
+                <div className='products-list'>
+                    {individualData?.products?.map((product)=>{
+                        return <ProductCard
+                                    key={product.id}
+                                    name={product.name}
+                                    price={product.price}
+                                    image={product.image}
+                                />
+                    })}
+                    
+                </div>
             </div>
             
+            }
             
         </div>
         
